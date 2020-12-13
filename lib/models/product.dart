5ae4e4924ef92bb96d44d0f03f23b1a7669eca07
davidhaas6 <davidhaas6@gmail.com>
2020-12-12 22:11:52 -0500

    finished product model and added images

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,26 @@
+// Data model for a product to watch
+
+enum ProductType { GPU, CPU, Console }
+
+Map iconMap = <ProductType, String>{
+  ProductType.CPU: 'assets/images/cpu.png',
+  ProductType.GPU: 'assets/images/gpu.png',
+  ProductType.Console: 'assets/images/console.png',
+};
+
 class Product {
   final String name, channel;
-  Product(this.name, this.channel);
+  final ProductType type;
+  Product(this.name, this.channel, this.type);
+
+  factory Product.fromJson(Map<String, dynamic> data, String firebaseID) {
+    // Maps string product type from firebase to the enum
+    ProductType productType = {
+      'cpu': ProductType.CPU,
+      'gpu': ProductType.GPU,
+      'console': ProductType.Console
+    }[data['type']];
+
+    return Product(data['name'], firebaseID, productType);
+  }
 }
